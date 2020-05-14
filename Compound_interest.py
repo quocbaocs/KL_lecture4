9874ac955e67e7b023410570fe3c02d3067d8618
quocbaole.cs <quocbao1997la@gmail.com>
2020-05-14 13:05:32 +0700

    first commit

--- Compound_interest.py
+++ Compound_interest.py
@@ -26,8 +26,22 @@ def calculate_compound_interest(p0, r, t):
         p.append(p0[i] * math.exp(r * t[i]))
     return p
 
+def Continuous_compounding(principle, rate, time):
+    CI = principle*np.exp((rate/100)*time)
+    return np.round(CI, 4)
+
+def Future_value(rate, nper, pmt, pv):
+    investment = np.fv(rate, nper, pmt, pv)
+    return investment
+def Bond_yeld(principle, rate, time):
+    for i in range(len(principle)):
+        P += principle*np.exp(-rate*time)
+    return P
+
 print(calculate_compound_interest([1, 2, 3], 0.5, [1, 10, 100]))
 # Driver Code
 print(compound_interest(1, 10, 2))
 print(compound_interest(100, 7, 10))
 print(monthly_compounding(20000, 8.5, 12, 4))
+print(Continuous_compounding(100, 5, 3))
+print("Future value: ", Future_value(0.08, 15, 0,10000))
